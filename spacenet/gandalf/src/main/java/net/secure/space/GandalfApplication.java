4ce0ab25a17ef11ec6dd6e444563ef34c26c9846
Ajay Menon <ajay.java.dev.@gmail.com>
2016-12-25 09:59:48 -0500

    base gandalf checkin

---
+++ spacenet/gandalf/src/main/java/net/secure/space/GandalfApplication.java
@@ -0,0 +1,23 @@
+package net.secure.space;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.cassandra.CassandraDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.XADataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, CassandraDataAutoConfiguration.class, XADataSourceAutoConfiguration.class})
+@ComponentScan(basePackages = "net.secure.space")
+@RestController
+public class GandalfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GandalfApplication.class, args);
+	}
+}
+
